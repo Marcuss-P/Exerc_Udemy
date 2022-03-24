6ed893efde3442ad1754c2bafd45e3e5e9a7d755
Marcus Paulo <marcuspaulorralves@outlook.com>
2022-03-24 09:33:30 -0300

    Abstração

---
+++ Exerc_classAbstract/src/entities/SavingsAccount.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class SavingsAccount extends Account{
+
+	private Double loanLimit;
+
+	public SavingsAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan(Double amount) {
+		
+	}
+	
+}
